fc277f47a8cd82e71a2b2c77e6a378f20e45989c
Prashant <rajprashantsharma@hotmail.com>
2022-09-01 00:14:32 +0100

    Upload json File

--- Utility.py
+++ Utility.py
@@ -1,16 +1,18 @@
-import numpy as np
-import pandas as pd
+#import numpy as np
+#import pandas as pd
 import json
 #from library.dbengine import DBEngine
 
 
+path ='/Users/prashantraj/Documents/Natural_Language_To_SQL_CE901_Project_Dissertation/WikiSQL_DataSet'
 
 def load_dataset(dataset_to_load):
     """Load the given dataset into memory."""
     print("Loading dataset : ", dataset_to_load)
-    sql_data_path = 'data/tokenized_' + dataset_to_load + '.jsonl'
-    table_data_path = 'data/tokenized_' + dataset_to_load + '.tables.jsonl'
-    db_file = 'data/' + dataset_to_load + '.db'
+    sql_data_path = 'WikiSQL_DataSet/' + dataset_to_load + '.jsonl'
+    table_data_path = 'WikiSQL_DataSet/' + dataset_to_load + '.tables.jsonl'
+    db_file = 'WikiSQL_DataSet/' + dataset_to_load + '.db'
+    print(sql_data_path)
 
     sql_data = []
     table_data = {}
@@ -29,4 +31,5 @@ def load_dataset(dataset_to_load):
 
 
 if __name__ == '__main__':
-  load_dataset('dataset_to_load')
+    sql_data, table_data, TRAIN_DB =load_dataset('dev')
+    print(TRAIN_DB)
