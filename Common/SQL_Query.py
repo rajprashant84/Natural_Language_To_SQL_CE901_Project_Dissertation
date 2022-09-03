75bc087e22c9dc46d8d9d315a5c0d31667bc129e
Prashant <rajprashantsharma@hotmail.com>
2022-09-03 16:26:54 +0100

    SQL lite Database connection

--- Common/SQL_Query.py
+++ Common/SQL_Query.py
@@ -8,14 +8,11 @@ from collections import defaultdict
 class SQL_Query:
     Agg_Command = ['SUM', 'MIN', 'MAX', 'COUNT', 'AVG', '']
     Conditional_Command = ['<', '>', '=', ]
-    Syntax =['SELECT', 'WHERE', 'AND','OR', 'COL', 'TABLE', 'CAPTION', 'PAGE', 'SECTION', 'OP', 'COND', 'QUESTION'
-        ,'AGG','AGGOPS', 'CONDOPS']
+    Syntax = ['SELECT', 'WHERE', 'AND', 'OR', 'COL', 'TABLE', 'CAPTION', 'PAGE', 'SECTION', 'OP', 'COND', 'QUESTION'
+        , 'AGG', 'AGGOPS', 'CONDOPS']
 
-
-    def __init__(self,select_index, agg_index, conditions=tuple(), ordered=False):
+    def __init__(self, select_index, agg_index, conditions=tuple(), ordered=False):
         self.select_index = select_index
         self.agg_index = agg_index
         self.conditions = list(conditions)
         self.ordered = ordered
-
-
