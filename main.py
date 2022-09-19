56ee31d7143c9c8034b07d57e814b389f48dfb03
Prashant <rajprashantsharma@hotmail.com>
2022-09-19 18:11:44 +0100

    Add new code

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from __future__ import unicode_literals, print_function, division
+from Model_training import train_model
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Running First train_model")
+    train_model.run_baseline()
